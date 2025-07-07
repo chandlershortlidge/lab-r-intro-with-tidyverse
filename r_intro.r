f25b86260104ece73e2fe8401679dd90de2d1052
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-07 13:17:49 +0200

    task 2 done

--- r_intro.r
+++ r_intro.r
@@ -1,5 +1,5 @@
-install.packages("tidyverse")
-library(tidyverse)
+# install.packages("tidyverse")
+# library(tidyverse)
 
 employees <- data.frame(
 		  ID = 1:6,
@@ -8,4 +8,33 @@ employees <- data.frame(
 		  Department = c("HR", "IT", "Finance", "IT", "HR", "Finance"),
 		  Salary = c(50000, 60000, 70000, 80000, 55000, 75000)
 		)
-print(c(employees))
+# print(c(employees))
+# 1. Filter rows: only "IT" department
+it_employees <- employees %>%
+  filter(Department == "IT")
+print(it_employees)
+
+# 2. Select columns: only Name and Salary
+name_salary <- employees %>%
+  select(Name, Salary)
+  print(name_salary)
+
+# 3. Add a new column: 10% Bonus based on Salary
+employees_with_bonus <- employees %>%
+  mutate(Bonus = Salary * 0.10)
+  print(employees_with_bonus)  
+
+
+# 4. Sort rows: by Salary in descending order
+sorted_employees <- employees %>%
+  arrange(desc(Salary))
+  print(sorted_employees)
+
+install.packages("utf8")
+
+
+# 5. Summarize data: average salary for each department
+avg_salary_by_dept <- employees %>%
+  group_by(Department) %>%
+  summarise(avg_salary = mean(Salary))
+print(avg_salary_by_dept)
